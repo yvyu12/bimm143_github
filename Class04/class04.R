cc63757e513e0393a44b63bbd9bab701673b284f
Yvonne Yu <yvyu@ucsd.edu>
2024-05-21 11:56:54 -0700

    Upload Class 4

---
+++ Class04/class04.R
@@ -0,0 +1,10 @@
+#' ---
+#' title: "BIMM 143 Class 4: R Basics"
+#' author: "Yvonne Yu (A16333006)"
+#' date: "April 11th, 2024"
+#' ---
+
+#This is just a text file
+x <- 1:50
+plot(x, sin(x))
+plot(x, sin(x), typ = "l", col = "violet", lwd=3)
